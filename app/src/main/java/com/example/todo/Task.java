751767fef025e88c33cc2266e33ebd966ebbdb37
gulumjan <zakirovagulumjan53@gmail.com>
2025-10-20 20:42:28 +0600

    I've added a lot of functionals and enhaced ihterface

--- app/src/main/java/com/example/todo/Task.java
+++ app/src/main/java/com/example/todo/Task.java
@@ -5,12 +5,16 @@ public class Task {
     private String title;
     private String description;
     private boolean isCompleted;
+    private String deadline;
+    private boolean isFavorite;
 
-    public Task(int id, String title, String description, boolean isCompleted) {
+    public Task(int id, String title, String description, boolean isCompleted, String deadline, boolean isFavorite) {
         this.id = id;
         this.title = title;
         this.description = description;
         this.isCompleted = isCompleted;
+        this.deadline = deadline;
+        this.isFavorite = isFavorite;
     }
 
     public int getId() {
@@ -21,8 +25,16 @@ public class Task {
         return title;
     }
 
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
     public String getDescription() {
         return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     public boolean isCompleted() {
@@ -31,5 +43,21 @@ public class Task {
 
     public void setCompleted(boolean completed) {
         isCompleted = completed;
+    }
+
+    public String getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(String deadline) {
+        this.deadline = deadline;
+    }
+
+    public boolean isFavorite() {
+        return isFavorite;
+    }
+
+    public void setFavorite(boolean favorite) {
+        isFavorite = favorite;
     }
 }
